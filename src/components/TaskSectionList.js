e8134adff1a3e9c851a7ae084b18dbb47a02c98c
Godsenal <tmqps78@gmail.com>
2018-02-19 23:25:05 +0900

    sticky section header manually

--- src/components/TaskSectionList.js
+++ src/components/TaskSectionList.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
-import { ScrollView } from 'react-native';
-import { TaskSectionListItem } from './';
+import { ScrollView, View, Text, StyleSheet } from 'react-native';
 
+import { TaskSectionListItem } from './';
+import { color } from '../config';
+
+const styles = StyleSheet.create({
+  header: {
+    height: 20,
+    padding: 20,
+    backgroundColor: color.whitegray,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+});
 
 export default class TaskSectionList extends Component {
   state = {
@@ -38,27 +50,35 @@ export default class TaskSectionList extends Component {
         ref={(ref) => { this._scrollRoot = ref; }}
         onLayout={this._onLayout}
         scrollEnabled={allowScroll}
+        stickyHeaderIndices={[0, 2, 4]}
       >
+        <View style={styles.header}>
+          <Text>오늘</Text>
+        </View>
         <TaskSectionListItem
-          header="오늘"
           scrollTo={this._scrollTo}
           containerHeight={layout.height}
           allowScroll={this._allowScroll}
           data={data.slice(0, 10)}
         />
+        <View style={styles.header}>
+          <Text>내일</Text>
+        </View>
         <TaskSectionListItem
-          header="내일"
           scrollTo={this._scrollTo}
           containerHeight={layout.height}
           allowScroll={this._allowScroll}
           data={data.slice(11, 20)}
         />
+        <View style={styles.header}>
+          <Text>다가오는</Text>
+        </View>
         <TaskSectionListItem
-          header="다가오는"
           scrollTo={this._scrollTo}
           containerHeight={layout.height}
           allowScroll={this._allowScroll}
-          data={data.slice(21, 30)}
+          data={data.slice(21, 50)}
+          isLast
         />
       </ScrollView>
     );
--- src/components/TaskSectionListItem.js
+++ src/components/TaskSectionListItem.js
@@ -1,30 +1,19 @@
 import React, { Component } from 'react';
-import { Animated, View, Text, ScrollView, TouchableOpacity, StyleSheet, Dimensions, findNodeHandle, UIManager } from 'react-native';
+import { Animated, View, Text, ScrollView, FlatList, TouchableOpacity, StyleSheet, findNodeHandle, UIManager } from 'react-native';
 import PropTypes from 'prop-types';
 
 import { DoubleTapWrapper } from './';
 import { color } from '../config';
 
-const DEFAULT_TRANSITION = 150;
+const DEFAULT_TRANSITION = 250;
 const DEFAULT_PADDING = 20;
-const WINDOW_WIDTH = Dimensions.get('window').width;
 const styles = StyleSheet.create({
   card: {
     borderRadius: 10,
-    marginTop: 10,
-    marginHorizontal: '5%',
     backgroundColor: 'white',
     overflow: 'hidden',
     borderWidth: 1,
     borderColor: color.whitegray,
-  },
-  cardHeader: {
-    height: 20,
-    padding: DEFAULT_PADDING,
-    backgroundColor: color.whitegray,
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
   },
   cardBody: {
     flex: 1,
@@ -48,14 +37,15 @@ export default class AnimatedCard extends Component {
       id: PropTypes.string.isRequired,
       text: PropTypes.string.isRequired,
     }).isRequired).isRequired,
-    header: PropTypes.string.isRequired,
     containerHeight: PropTypes.number.isRequired,
     scrollTo: PropTypes.func,
     allowScroll: PropTypes.func,
+    isLast: PropTypes.bool,
   }
   static defaultProps = {
     scrollTo: () => {},
     allowScroll: () => {},
+    isLast: false,
   }
   state = {
     data: this.props.data,
@@ -82,9 +72,14 @@ export default class AnimatedCard extends Component {
     const willOpacity = this.state.isInit ? 1 : 0;
     this._isAnimating = true;
 
-    this.setState({
-      isInit: !this.state.isInit,
-    }, () => {
+    const handle = findNodeHandle(this._root.getNode());
+    UIManager.measureLayoutRelativeToParent(handle, (err) => { console.error(err); }, (ox, oy) => {
+      if (!this.state.isInit) {
+        this.props.allowScroll(true);
+      } else {
+        this.props.allowScroll(false);
+        this.props.scrollTo(oy - 40);
+      }
       Animated.timing(this.state.opacity, {
         toValue: willOpacity,
         duration: DEFAULT_TRANSITION,
@@ -93,47 +88,19 @@ export default class AnimatedCard extends Component {
   }
   _onAnimationEnd = () => {
     this._isAnimating = false;
-    const handle = findNodeHandle(this._root.getNode());
-    UIManager.measureLayoutRelativeToParent(handle, (err) => { console.error(err); }, (ox, oy) => {
-      if (this.state.isInit) {
-        this.props.allowScroll(true);
-      } else {
-        this.props.allowScroll(false);
-        this.props.scrollTo(oy);
-      }
+    this.setState({
+      isInit: !this.state.isInit,
     });
   }
-  render() {
-    const { isInit, opacity, data } = this.state;
-    const { header, containerHeight } = this.props;
-    const containerStyle = [styles.card, {
-      marginHorizontal: opacity.interpolate({
-        inputRange: [0, 1],
-        outputRange: [WINDOW_WIDTH * 0.05, 0],
-      }),
-    }];
-    const headerStyle = [styles.cardHeader, {
-      height: opacity.interpolate({
-        inputRange: [0, 1],
-        outputRange: [20, 30],
-      }),
-    }];
-    const bodyStyle = [styles.cardBody, {
-      height: opacity.interpolate({
-        inputRange: [0, 1],
-        outputRange: [200, containerHeight - (40 + (DEFAULT_PADDING * 2))],
-      }),
-    }];
-    return (
-      <Animated.View style={containerStyle} ref={(ref) => { this._root = ref; }}>
-        <Animated.View style={headerStyle}>
-          <Text>{header}</Text>
-        </Animated.View>
-        <Animated.View style={bodyStyle}>
-          <ScrollView
-            scrollEnabled={!isInit}
-          >
-            {data.slice(0, isInit || this._isAnimating ? 3 : 10).map(item => (
+  _renderBody = () => {
+    const { data } = this.state;
+    const { isLast } = this.props;
+    if (isLast) {
+      return (
+        <View style={styles.cardBody}>
+          <FlatList
+            data={data}
+            renderItem={({ item }) => (
               <DoubleTapWrapper
                 key={item.id}
                 {...item}
@@ -144,14 +111,56 @@ export default class AnimatedCard extends Component {
                   {item.text}
                 </Text>
               </DoubleTapWrapper>
-            ))}
-          </ScrollView>
-        </Animated.View>
-        <View style={styles.cardFooter}>
-          <TouchableOpacity onPress={this._onTap}>
-            <Text style={{ textAlign: 'right' }}>{isInit ? '더 보기' : '최소화'}</Text>
-          </TouchableOpacity>
+            )}
+            keyExtractor={item => (item.id)}
+            extraData={this.state.removeQueue}
+          />
         </View>
+      );
+    }
+    const { isInit, opacity } = this.state;
+    const { containerHeight } = this.props;
+    const bodyStyle = [styles.cardBody, {
+      height: opacity.interpolate({
+        inputRange: [0, 1],
+        outputRange: [200, containerHeight - (40 + (DEFAULT_PADDING * 2))],
+      }),
+    }];
+    return (
+      <Animated.View style={bodyStyle}>
+        <ScrollView
+          scrollEnabled={!isInit}
+        >
+          {data.slice(0, isInit || this._isAnimating ? 3 : 10).map(item => (
+            <DoubleTapWrapper
+              key={item.id}
+              {...item}
+              onDoubleTap={() => this._onDoubleTap(item.id)}
+              onAnimationEnd={() => this._removeItem(item.id)}
+            >
+              <Text style={{ color: 'black' }}>
+                {item.text}
+              </Text>
+            </DoubleTapWrapper>
+          ))}
+        </ScrollView>
+      </Animated.View>
+    );
+  }
+  render() {
+    const { isInit } = this.state;
+    const { isLast } = this.props;
+    return (
+      <Animated.View style={styles.card} ref={(ref) => { this._root = ref; }}>
+        {this._renderBody()}
+        {
+          isLast ? null :
+          <View style={styles.cardFooter}>
+            <TouchableOpacity onPress={this._onTap}>
+              <Text style={{ textAlign: 'right' }}>{isInit ? '더 보기' : '최소화'}</Text>
+            </TouchableOpacity>
+          </View>
+        }
       </Animated.View>
     );
   }
